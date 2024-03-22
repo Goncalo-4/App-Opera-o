07bf663fc3d174ff3fe7433e6ee6c08f3b7e34fe
Goncalo-4 <gonxas_pires@hotmail.com>
2024-03-22 14:07:23 +0000

    first app

---
+++ js.js
@@ -0,0 +1,25 @@
+function getMilf(bottles) {
+console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("Buy" + bottles + "bottles of milk")
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+};
+
+getMilf(12);
+
+
